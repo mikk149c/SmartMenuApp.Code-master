1f4dcf18266fca276f318b6ef24b0b7ce6315dd5
Mikkel Jørgensen <mikk149c@edu.eal.dk>
2018-09-27 15:24:03 +0200

    Cleaned up code with better comments

--- SmartMenuLibrary/SmartMenu.cs
+++ SmartMenuLibrary/SmartMenu.cs
@@ -15,48 +15,64 @@ namespace SmartMenuLibrary
         public void LoadMenu(string path)
         {
 			getprintableStringAndActionDictionary(
-				new StreamReader($"../../{path}").ReadToEnd(),
-				out menu,
-				out menuActions);
+				new StreamReader($"../../{path}").ReadToEnd());
         }
-        public void Activate()
-        {
-			Console.Write(menu);										//Print menu
-
-			char userChoice = Console.ReadKey().KeyChar;				//Gets user choice
-			Action action;												//Creates and empty action
-			while (!menuActions.TryGetValue(userChoice, out action))	//Try to get action for the current user input otherwhise
-			{
-				Console.WriteLine("Invalid menu action try again");
-				userChoice = Console.ReadKey().KeyChar;						//Get a new user input
-			}
-			action();                                                   //preform action
-			Console.ReadKey();
-        }
-
-		internal void getprintableStringAndActionDictionary(
-			string line,
-			out string print,
-			out Dictionary<char, Action> menuActions)
+		internal void getprintableStringAndActionDictionary(string line)
 		{
+			menu = "";
+			menuActions = new Dictionary<char, Action>();
+			//Create an array of the lines from the menu string
+			string[] menuLineArray = line.Split('\n');
+			getSmartMenuDescriptionAndExitAction(menuLineArray);
 
-			print = "";													//Initialize print
-			menuActions = new Dictionary<char, Action>();				//Initialize menuActions
-			string[] menuLineArray = line.Split('\n');                  //Create an array of the lines from the mu string
-			print =														//Add the first two lines to print
+			getMenuPointTextAndActions(menuLineArray);
+		}
+
+		private void getSmartMenuDescriptionAndExitAction(string[] menuLineArray)
+		{
+			//Add the first two lines to menu string
+			menu =
 				$"{menuLineArray[0]}\n" +
 				$"{menuLineArray[1]}\n";
+			//Add the exit action to menuActions
+			menuActions.Add('0', () => Console.WriteLine("exit"));
+		}
 
-			menuActions.Add('0', () => Console.WriteLine("exit"));		//Add the exit action to menuAvtions
-
-			for (int i = 2; i < menuLineArray.Length; i++)              //for each line after the first two
+		private void getMenuPointTextAndActions(string[] menuLineArray)
+		{
+			for (int i = 2; i < menuLineArray.Length; i++)
 			{
-				string[] textAndMenuID = menuLineArray[i].Split(';');       //Split the current line into menu point text and ID
-				print += $"{textAndMenuID[0]}\n";                           //Adds menu point text to print string
-				menuActions.Add(											//Add action to menuActions
+				//Split the current line into menu point text and ID
+				string[] textAndMenuID = menuLineArray[i].Split(';');
+				menu += $"{textAndMenuID[0]}\n";
+				//Add action to menuActions
+				menuActions.Add(
 					Convert.ToChar(textAndMenuID[0].Split(' ')[1]),
 					() => Console.WriteLine("\nBindings.run(textAndMenuID[1])"));//waiting for Bindings to be implemented
 			}
 		}
-    }
+
+		public void Activate()
+		{
+			Console.Write(menu);
+			Action action = getValidUserAction();
+			//preform action
+			action();
+			Console.ReadKey();
+		}
+
+		private Action getValidUserAction()
+		{
+			Action action;
+			char userChoice = Console.ReadKey().KeyChar;
+			//Try to get action for the current user input otherwhise
+			while (!menuActions.TryGetValue(userChoice, out action))
+			{
+				Console.WriteLine("Invalid menu action try again");
+				userChoice = Console.ReadKey().KeyChar;
+			}
+
+			return action;
+		}
+	}
 }

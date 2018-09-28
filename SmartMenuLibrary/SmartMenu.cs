4c3aef3b9228921d47ce151287c0a5021ff2b28e
Louise Hyldal Thingholm Hansen <loui629f@edu.eal.dk>
2018-09-28 11:05:13 +0200

    Switch

--- SmartMenuLibrary/SmartMenu.cs
+++ SmartMenuLibrary/SmartMenu.cs
@@ -15,6 +15,20 @@ namespace SmartMenuLibrary
         public void Activate()
         {
             // Implement ...
+            int caseSwitch = 1;
+
+            switch (caseSwitch)
+             {
+
+                case 1:
+                    Console.WriteLine("Do This");
+                    break;
+                case 2:
+                    Console.WriteLine("Do That");
+                    break;
+                case 3:
+                    Console.WriteLine("Do Something");
+}
         }
     }
 }

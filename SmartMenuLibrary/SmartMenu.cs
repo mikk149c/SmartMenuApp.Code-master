ced16bab06dca813f72007cf2151b32de91e0c1e
Anders Dalsgaard Madsen <ande48d0@edu.eal.dk>
2018-09-28 12:05:04 +0200

    Load af MenuSpec.txt samt valg af enten dansk eller engelsk.

--- SmartMenuLibrary/SmartMenu.cs
+++ SmartMenuLibrary/SmartMenu.cs
@@ -8,13 +8,34 @@ namespace SmartMenuLibrary
 {
     public class SmartMenu
     {
+        string[] bothMenu;
         public void LoadMenu(string path)
         {
-            // Implement ...
+            System.IO.StreamReader file =
+    new System.IO.StreamReader(@"c:..\..\" + path + "");
+            bothMenu = file.ReadToEnd().Split('\n');
         }
         public void Activate()
         {
-            // Implement ...
+            Console.WriteLine("Vælg dit sprog // Choose your language!\n1. Dansk\n2. English");
+            string menuChoice = Console.ReadLine();
+            if (int.Parse(menuChoice) == 1)
+            {
+                Console.Clear();
+                for (int i = 0; i < 5; i++)
+                {
+                    Console.WriteLine(bothMenu[i]);
+                }
+            }
+            else if (int.Parse(menuChoice) == 2)
+            {
+                Console.Clear();
+                for (int i = 5; i < 10; i++)
+                {
+                    Console.WriteLine(bothMenu[i]);
+                }
+            }
+            Console.ReadLine();
         }
     }
 }
